0aa37dae21f4a796e563efd8820cdf96567b019a
Karim Agha <karim.dev@gmail.com>
2021-03-07 22:48:49 +0000

    adding scratchpad crate

---
+++ tools/scratchpad/src/main.rs
@@ -0,0 +1,14 @@
+// Copyright 2021 The OpenEthereum Authors.
+// Licensed under the Apache License, Version 2.0.
+
+/// This executable is used during development for experimenting
+/// with any component under development with all dependencies 
+/// included. 
+///
+/// After the initial commit, all changes inside the scratchpad 
+/// crate are ignore by git through .gitignore as the content
+/// of this crate are developer private scratchpad area.
+
+fn main() {
+  println!("Running inside OpenEthereum 4.0 environment");
+}
