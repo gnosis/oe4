28ae1c1d008488d53f9c71ceae7c2172cbf7a835
Karim Agha <karim.dev@gmail.com>
2021-03-07 01:33:16 +0000

    cleanup and reuse common-types

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,14 +1,17 @@
 // Copyright 2021 The OpenEthereum Authors.
 // Licensed under the Apache License, Version 2.0.
 
+mod account;
 mod block;
-mod hash;
 mod transaction;
-mod uint;
 
-pub use crate::hash::{H128, H160, H256, H264, H512};
-pub use crate::hash::{H160 as Address, H256 as Keccak};
-pub use crate::uint::{U128, U256, U512};
+// large integers
+pub use ethereum_types::U256;
 
+// special purpose hashes
+pub use ethereum_types::{Address, Bloom, H256 as Keccak};
+
+// domain types
+pub use account::Account;
 pub use block::Block;
 pub use transaction::Transaction;
